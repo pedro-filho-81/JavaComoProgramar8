b857186ee2c00e673ebaf6f401e57049a9bd9f15
Pedro Filho <61125636+pedro-filho-81@users.noreply.github.com>
2021-01-11 16:12:32 -0300

    Texto com printf()
    Exibe umtexto formatado com System.out.printf( )

---
+++ TextoComPrintf/src/textocomprintf/TextoComPrintf.java
@@ -0,0 +1,18 @@
+package textocomprintf;
+
+/**
+ * Exibe texto formatado com System.out.printf();
+ * Autor Pedro Filho, 11/01/2021
+ */
+
+// classe Texto com printif
+public class TextoComPrintf {
+
+    // função principal
+    public static void main(String[] args) {
+        // imprime formatado
+        // %s exebe uma string
+        // %n pula linha
+        System.out.printf("%n%s%n%n%s%n", "Bem vindo para:", "Programação Java!" );
+    } // fim main
+} // fim classe
