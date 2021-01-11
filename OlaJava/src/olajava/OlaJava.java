026d9227c9b7eede41c82b80af3efd7c5d73cf04
Pedro Filho <61125636+pedro-filho-81@users.noreply.github.com>
2021-01-11 15:25:59 -0300

    Olá Mundo Java
    Primeiro programa em java

---
+++ OlaJava/src/olajava/OlaJava.java
@@ -0,0 +1,18 @@
+package olajava;
+
+/**
+ * Primeiro programa Java
+ * Mostrar olá mundo na tela * 
+ * @author Pedro Filho, 11/01/2021
+ */
+
+// classe olá mundo
+public class OlaJava {
+
+   // função principal
+    public static void main(String[] args) {
+        
+        // mostra na tela
+        System.out.println( "Olá, Mundo Java." );
+    } // fim main    
+} // fim classe
