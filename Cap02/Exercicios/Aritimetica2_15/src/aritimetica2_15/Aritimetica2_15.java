7f52bff918285abaa92bd0fd080792192f25d210
Pedro Filho <61125636+pedro-filho-81@users.noreply.github.com>
2021-01-26 15:39:53 -0300

    Aritimética

---
+++ Cap02/Exercicios/Aritimetica2_15/src/aritimetica2_15/Aritimetica2_15.java
@@ -0,0 +1,37 @@
+package aritimetica2_15;
+// importar biblioteca Scanner
+import java.util.Scanner;
+
+public class Aritimetica2_15 {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Scanner input = new Scanner( System.in );
+        
+        // variáveis
+        int num1 = 0; // para o primeiro número
+        int num2 = 0; // para o segundo número
+        int somar = 0;
+        int diminuir = 0;
+        int multiplicar = 0;
+        int dividir = 0;
+        
+        // entrada de dados
+        System.out.print("Digite dois inteiros: " ); // prompt
+        num1 = input.nextInt();
+        num2 = input.nextInt();
+        
+        // calcular
+        somar = num1 + num2;
+        diminuir = num1 - num2;
+        multiplicar = num1 * num2;
+        dividir = num1 / num2;
+        
+        // mostrar resultado
+        System.out.printf( "Somar: %d + %d = %d\n", num1, num2, somar );
+        System.out.printf( "Diminuir: %d - %d = %d\n", num1, num2, diminuir );
+        System.out.printf( "Multiplicar %d * %d = %d\n", num1, num2, multiplicar );
+        System.out.printf( "Dividir: %d / %d = %d\n\n", num1, num2, dividir );
+        
+    } // fim main    
+} // fim classe
