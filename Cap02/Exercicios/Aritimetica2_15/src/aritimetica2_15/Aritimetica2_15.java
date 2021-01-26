0d6540a06f1237fbd8d847bca3afbcf68a4ca610
Pedro Filho <61125636+pedro-filho-81@users.noreply.github.com>
2021-01-26 15:51:59 -0300

    Update Aritimetica2_15.java

--- Cap02/Exercicios/Aritimetica2_15/src/aritimetica2_15/Aritimetica2_15.java
+++ Cap02/Exercicios/Aritimetica2_15/src/aritimetica2_15/Aritimetica2_15.java
@@ -1,3 +1,10 @@
+/**
+ * Questão: 2.15 (Aritmética) Escreva um aplicativo que solicite ao usuário 
+ * inserir dois inteiros, obtenha dele esses números e imprima sua soma, produto,  
+ * diferença e quociente (divisão). Utilize as técnicas mostradas na Figura 2.7. 
+   Deitel, Paul; Deitel, Harvey. Java: como programar (p. 52). Edição do Kindle. 
+ *  @PedroFilho, 26/20/21.
+*/
 package aritimetica2_15;
 // importar biblioteca Scanner
 import java.util.Scanner;
@@ -15,6 +22,7 @@ public class Aritimetica2_15 {
         int diminuir = 0;
         int multiplicar = 0;
         int dividir = 0;
+        int resto = 0;
         
         // entrada de dados
         System.out.print("Digite dois inteiros: " ); // prompt
@@ -26,12 +34,14 @@ public class Aritimetica2_15 {
         diminuir = num1 - num2;
         multiplicar = num1 * num2;
         dividir = num1 / num2;
+        resto = num1 % num2;
         
         // mostrar resultado
         System.out.printf( "Somar: %d + %d = %d\n", num1, num2, somar );
         System.out.printf( "Diminuir: %d - %d = %d\n", num1, num2, diminuir );
         System.out.printf( "Multiplicar %d * %d = %d\n", num1, num2, multiplicar );
-        System.out.printf( "Dividir: %d / %d = %d\n\n", num1, num2, dividir );
+        System.out.printf( "Dividir: %d / %d = %d\n", num1, num2, dividir );
+        //System.out.printf( "Resto: %d % %d = %d\n\n", num1, num2, resto );
         
     } // fim main    
 } // fim classe
