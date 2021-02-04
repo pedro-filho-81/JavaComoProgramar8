ff4209d529b290f440880a8b083ab2c87a933545
Pedro Filho <61125636+pedro-filho-81@users.noreply.github.com>
2021-02-04 15:43:17 -0300

    Create Exercicio2_20.java

---
+++ Cap02/Exercicios/Exercicio2_20/src/exercicio2_20/Exercicio2_20.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package exercicio2_20;
+
+/**
+ *
+ * @author samsung
+ */
+public class Exercicio2_20 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // imprimir
+        System.out.println("*");
+        System.out.println("***");
+        System.out.println("*****");
+        System.out.println("****");
+        System.out.println("***");
+                
+    }
+    
+}
