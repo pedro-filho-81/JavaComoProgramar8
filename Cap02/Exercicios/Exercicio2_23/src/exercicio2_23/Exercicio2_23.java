0c7cdf2d0f30011ff4abc27ee89dde324119463c
Pedro Filho <61125636+pedro-filho-81@users.noreply.github.com>
2021-02-04 15:41:47 -0300

    Create Exercicio2_23.java

---
+++ Cap02/Exercicios/Exercicio2_23/src/exercicio2_23/Exercicio2_23.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package exercicio2_23;
+
+/**
+ *
+ * @author samsung
+ */
+public class Exercicio2_23 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.printf( "%s\n%s\n%s\n", "*", "***", "*****" );
+    }
+    
+}
